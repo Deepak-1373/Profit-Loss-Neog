51df528a44ed3c6384b0b6c72fe60da87a29ee7a
Deepak Kamat <dkamat.001@gmail.com>
2021-09-22 11:23:34 +0530

    Functionality: All Fields are fetched

---
+++ index.js
@@ -0,0 +1,5 @@
+const initialPrice = document.querySelector("#initial-price");
+const stocksQuantity = document.querySelector("#stocks-quantity");
+const currentPrice = document.querySelector("#current-price");
+const submitBtn = document.querySelector("#submt-btn");
+const outputDiv = document.querySelector("#output");
